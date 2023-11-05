c95df59273768681824b96a691c364a8fa6124dc
AmalAlshehrig <amalalshehrig@gmail.com>
2023-11-05 15:54:39 +0300

    add

---
+++ lab.js
@@ -0,0 +1,26 @@
+/*let num1=9
+let num2=9
+let num3
+let sum=document.getElementById("demo")
+num3=num1+num2
+sum.innerText =`${num1} + ${num2} = ${num3}`;
+let sab=document.getElementById("demo1")
+num3=num1-num2
+sab.innerText =`${num1} - ${num2} = ${num3}`;
+let mult=document.getElementById("demo2")
+num3=num1*num2
+mult.innerText =`${num1} * ${num2} = ${num3}`;
+let dav=document.getElementById("demo3")
+num3=num1/num2
+dav.innerText =`${num1} / ${num2} = ${num3}`;*/
+//============================================================
+/*let str="Amal Ali Alshehri"
+const reversedString = str.split("").reduce((acc, char) => char + acc, "");
+let text1=document.getElementById("text1")
+text1.innerText=reversedString
+let result = str.toUpperCase();
+let text2=document.getElementById("text2")
+text2.innerText=result
+let result2 = str.toLowerCase();
+let text3=document.getElementById("text3")
+text3.innerText=result2*/
